0623d28a282984fd7c10e5dde967c8a32cc0a632
Sillycatlover246 <Sillycatlover246@gmail.com>
2025-11-30 16:15:50 +0200

    Randomized bucket names

--- main.tf
+++ main.tf
@@ -1,18 +1,24 @@
 terraform {
- required_providers {
-  aws = {
-   source  = "hashicorp/aws"
-   version = "~>5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
   }
- }
 }
 
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
+}
+
+resource "random_string" "suffix" {
+  length  = 6
+  upper   = false
+  special = false
 }
 
 module "ci_bucket" {
- source      = "./modules/s3_bucket"
- bucket_name = "silly-day8-ci"
- purpose     = "CI/CD Terraform demo"
+  source      = "./modules/s3_bucket"
+  bucket_name = "silly-day8-ci-${random_string.suffix.result}"
+  purpose     = "CI/CD Terraform demo"
 }
